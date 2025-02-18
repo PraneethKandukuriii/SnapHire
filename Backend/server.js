907dc3577a365e25dbe673e7e2ea50686802e2c0
Praneeth Kumar Kandukuri <praneethkumarkandukuri@Praneeths-MacBook-Air.local>
2025-02-18 12:24:16 +0530

    User registration done

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
 const http = require('http');   
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
 
 const server = http.createServer(app);
 
-server.listen(port,()=>{
+server.listen(8000,()=>{
     console.log(`Server is running on port ${port}`);
 });
+
